e2896fc2ea9ca0fed8cb1c5ebd8b9544aa1f587a
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-10-24 19:49:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function preload(){
 function setup(){
     canvas=createCanvas(640,480);
     canvas.position(110,250);
-    video=captureCapture(VIDEO);
+    video=createCapture(VIDEO);
     video.hide();
 
     tint_color="";
